5faf8297ab5d77da9e416505a2d7c4fc0366fc93
trongthienpc <pcking60@gmail.com>
2022-02-06 15:44:03 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const authRoute = require('./routes/auth')
+
+require('dotenv').config()
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@bme-admin.5yuf9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+        console.log('Mongoose connected');
+
+    } catch (error) {
+        console.log(error.message);
+        process.exit(1)
+    }
+}
+
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors())
+
+app.use('/api/auth', authRoute)
+
+const PORT = process.env.PORT || 5001
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
---
+++ routes/auth.js
@@ -0,0 +1,102 @@
+const express = require('express')
+const router = express.Router()
+const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
+
+const verifyToken = require('../middleware/auth')
+
+const User = require('../models/user')
+
+// get
+router.get('/', verifyToken, async (req, res) => {
+    try {
+        const user = await User.findById(req.userId).select('-password')
+        if (!user)
+            return res.status(400).json({ success: false, message: 'user not found' })
+        res.json({ success: true, user })
+    } catch (error) {
+
+    }
+})
+
+// post
+router.post('/register', async (req, res) => {
+
+    const { username, password } = req.body
+    if (!username || !password)
+        return res.status(400).json({ success: false, message: 'Missing username and/or password' })
+
+    try {
+        const user = await User.findOne({ username })
+        if (user)
+            return res
+                .status(400)
+                .json({ success: false, message: 'username already taken' })
+
+        const hashedPassword = await argon2.hash(password)
+        const newUser = new User({ username, password: hashedPassword })
+        await newUser.save()
+
+        const accessToken = jwt.sign({
+            userId: newUser._id
+        }, process.env.ACCESS_TOKEN_SECRET)
+
+        res.json({
+            success: true,
+            message: 'User created successfully',
+            accessToken
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: 'Có lỗi gì đó' })
+    }
+})
+
+// login
+
+router.post('/login', async (req, res) => {
+
+    const { username, password } = req.body
+
+    if (!username || !password)
+        return res
+            .status(400)
+            .json({ success: false, message: 'Missing username and/or password' })
+
+    try {
+        const user = await User.findOne({ username })
+        if (!user)
+            return res
+                .status(400)
+                .json({
+                    success: false,
+                    message: 'Incorrect username or password'
+                })
+
+        const passwordValid = await argon2.verify(user.password, password);
+        if (!passwordValid)
+            return res
+                .status(400)
+                .json({
+                    success: false,
+                    message: 'Incorrect username or password'
+                })
+
+        const accessToken = jwt.sign({ userId: user._id }, process.env.ACCESS_TOKEN_SECRET)
+        res
+            .json({
+                success: true,
+                message: 'Đăng nhập thành công',
+                accessToken
+            })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: 'Internal server error' })
+    }
+
+
+})
+
+
+module.exports = router
