ef19041203961b2337e7da120a1bac6b2df8c23e
thientt <thientt.bdst@gmail.com>
2022-02-11 17:41:59 +0700

    macos

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const authRoute = require('./routes/auth');
-const roomStyleRoute = require('./routes/room');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authRoute = require("./routes/auth");
+const roomRoute = require("./routes/room");
 
-require('dotenv').config()
+require("dotenv").config();
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@bme-admin.5yuf9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
-        console.log('Mongoose connected');
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@bme-admin.5yuf9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+    );
+    console.log("Mongoose connected");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
 
-    } catch (error) {
-        console.log(error.message);
-        process.exit(1)
-    }
-}
+connectDB();
 
-connectDB()
+const app = express();
 
-const app = express()
+app.use(express.json());
 
-app.use(express.json())
+app.use(cors());
 
-app.use(cors())
+app.use("/api/auth", authRoute);
+app.use("/api/rooms", roomRoute);
 
-app.use('/api/auth', authRoute)
-app.use('/api/room', roomStyleRoute)
+const PORT = process.env.PORT || 5001;
 
-const PORT = process.env.PORT || 5001
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
--- routes/room.js
+++ routes/room.js
@@ -1,63 +1,74 @@
-const express = require('express');
-const multer = require('multer');
-const router = express.Router()
-let RoomStyle = require('../models/roomStyle')
-const path = require('path');
-const upload = multer(
-    {
-        limits: {
-            fileSize: 10000000
-        },
-        fileFilter(req, file, cb) {
-            var filetypes = /jpeg|jpg|png/;
-            var mimetype = filetypes.test(file.mimetype);
-            var extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+const express = require("express");
+const multer = require("multer");
+const router = express.Router();
+let RoomStyle = require("../models/roomStyle");
+const path = require("path");
+const verifyToken = require("../middleware/auth");
+// upload image
+const upload = multer({
+  limits: {
+    fileSize: 10000000,
+  },
+  fileFilter(req, file, cb) {
+    var filetypes = /jpeg|jpg|png/;
+    var mimetype = filetypes.test(file.mimetype);
+    var extname = filetypes.test(path.extname(file.originalname).toLowerCase());
 
-            if (mimetype && extname) {
-                return cb(null, true);
-            }
-            cb("Error: File upload only supports the following filetypes - " + filetypes);
-        }
-    })
-
-router.post("/add", upload.single('image'), async (req, res) => {
-    try {
-        const name = req.body.name;
-        const max = req.body.max
-        const bed = req.body.bed
-        const description = req.body.description
-        const createdBy = 'author' // edit later
-        const image = req.file.buffer
-
-        const newRoomStyleData = {
-            name,
-            max,
-            bed,
-            description,
-            createdBy,
-            image
-        }
-        console.log(newRoomStyleData);
-
-        const newRoomStyle = new RoomStyle(newRoomStyleData)
-
-        newRoomStyle
-            .save()
-            .then(() => {
-                res.json({
-                    status: true,
-                    message: "New Room Style added successfully!"
-                })
-            })
-    } catch (error) {
-        res
-            .status(400)
-            .json({
-                success: false,
-                message: error.message
-            })
+    if (mimetype && extname) {
+      return cb(null, true);
     }
+    cb(
+      "Error: File upload only supports the following filetypes - " + filetypes
+    );
+  },
+});
 
-})
+// list all room style
+router.get("/all", async (req, res) => {
+  try {
+    console.log("calling room api");
+    const response = await RoomStyle.find({});
 
-module.exports = router
+    res.json({ success: true, response });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// add new room style
+router.post("/add", upload.single("image"), async (req, res) => {
+  try {
+    const name = req.body.name;
+    const max = req.body.max;
+    const bed = req.body.bed;
+    const description = req.body.description;
+    const createdBy = "author"; // edit later
+    const image = req.file.buffer;
+
+    const newRoomStyleData = {
+      name,
+      max,
+      bed,
+      description,
+      createdBy,
+      image,
+    };
+    console.log(newRoomStyleData);
+
+    const newRoomStyle = new RoomStyle(newRoomStyleData);
+
+    newRoomStyle.save().then(() => {
+      res.json({
+        status: true,
+        message: "New Room Style added successfully!",
+      });
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+module.exports = router;
